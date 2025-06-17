cb776850543730c47b8217d24617fbd96ce37577
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-06-17 23:41:44 +0200

    update script bash to run tmux

--- exploratory_notebooks/running_script.sh
+++ exploratory_notebooks/running_script.sh
@@ -1,3 +1,5 @@
-tmux new -d -s jobeurosatcontrastive 
-mkdir -p runs
-bash -lc 'conda activate myenv && python eurosat_contrastive_stratified.py > runs/jobeurosatcontrastive.log 2>&1'
+#!/bin/bash
+jupyter nbconvert --to script eurosat_contrastive_stratified.ipynb
+tmux new -d -s jobeurosatcontrastive-aug-reploss-2
+tmux attach -t jobeurosatcontrastive-aug-reploss-2
+conda activate myenv && python eurosat_contrastive_stratified.py > runs/jobeurosatcontrastive-aug-reploss-2.log 2>&1
