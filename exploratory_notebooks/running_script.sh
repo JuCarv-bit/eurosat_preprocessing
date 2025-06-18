e049da927849c367374b87f498644041098c5271
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-06-19 01:12:50 +0200

    add line  to  script

--- exploratory_notebooks/running_script.sh
+++ exploratory_notebooks/running_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # jupyter nbconvert --to script eurosat_contrastive_stratified.ipynb
-jupyter nbconvert --to script eurosat_contrastive.ipynb --output eurosat_contrastive-bsrep32
-tmux new-session -As jobeurosatcontrastive-aug-reploss-2-bsrep32
-conda activate myenv && python eurosat_contrastive-bsrep32.py > runs/jobeurosat_contrastive-bsrep32.log 2>&1
+conda  activate myenv
+cd /users/c/carvalhj/projects/eurosat_preprocessing/exploratory_notebooks
+jupyter nbconvert --to script eurosat_contrastive.ipynb --output eurosat_contrastive-bs256_ep100
+tmux new-session -As jobeurosatcontrastive-aug-reploss-2-bs256_ep100
+conda activate myenv && python eurosat_contrastive-bs256_ep100.py > runs/jobeurosat_contrastive-bs256_ep100.log 2>&1
