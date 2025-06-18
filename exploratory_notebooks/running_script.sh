d334dd58896f7d2e3d4ea68071783b03981e4934
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-06-18 20:17:31 +0200

    update runing script

--- exploratory_notebooks/running_script.sh
+++ exploratory_notebooks/running_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # jupyter nbconvert --to script eurosat_contrastive_stratified.ipynb
-jupyter nbconvert --to script eurosat_contrastive.ipynb --output eurosat_contrastive-bs256
-tmux new-session -As jobeurosatcontrastive-aug-reploss-2-bs256
-conda activate myenv && python eurosat_contrastive-bs256.py > runs/jobeurosat_contrastive-bs256.log 2>&1
+jupyter nbconvert --to script eurosat_contrastive.ipynb --output eurosat_contrastive-bsrep32
+tmux new-session -As jobeurosatcontrastive-aug-reploss-2-bsrep32
+conda activate myenv && python eurosat_contrastive-bsrep32.py > runs/jobeurosat_contrastive-bsrep32.log 2>&1
