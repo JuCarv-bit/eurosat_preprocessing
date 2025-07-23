db51ee5a83a28064b78f05067a9056da66498e43
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-07-23 11:01:16 +0200

    original yaware run (91.56%)

--- exploratory_notebooks/simclr/config.py
+++ exploratory_notebooks/simclr/config.py
@@ -12,9 +12,9 @@ CONFIG = {
     "LR": 3.75e-4,
     "WD": 0.5,
     "LR_LINEAR": 3.75e-4,
-    "EPOCHS_SIMCLR": 2,
-    "EPOCHS_LINEAR": 2,
-    "EPOCH_SAVE_INTERVAL": 1,
+    "EPOCHS_SIMCLR": 100,
+    "EPOCHS_LINEAR": 20,
+    "EPOCH_SAVE_INTERVAL": 10,
     "TEMPERATURE": 0.2,
     "PROJ_DIM": 64,
     "FEATURE_DIM": 512, # ResNet50 feature dimension = 2048, ResNet18 feature dimension = 512
