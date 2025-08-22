fdfa4bedb142ffec1098d75692c820d7f8a98510
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-08-22 16:12:14 +0200

    Rename evaluate script

--- exploratory_notebooks/notebooks/evaluate.sh
+++ exploratory_notebooks/notebooks/evaluate.sh
@@ -1,7 +1,7 @@
 mkdir -p /share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights
 # --output-dir "/share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights" \
 
-python /share/homes/carvalhj/projects/eurosat_preprocessing/exploratory_notebooks/notebooks/main_evaluate.py \
+python /share/homes/carvalhj/projects/eurosat_preprocessing/exploratory_notebooks/notebooks/main.py \
   --weights "/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-06-21/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth" \
   --model resnet18 \
   --dataset "eurosat" \
