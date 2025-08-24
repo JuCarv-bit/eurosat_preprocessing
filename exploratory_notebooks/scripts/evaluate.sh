ed8dc4ef34ba1fe3fc50a52905878f84dd98c40a
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-08-24 17:53:16 +0200

    add pmodels path to evaluation schema

--- exploratory_notebooks/scripts/evaluate.sh
+++ exploratory_notebooks/scripts/evaluate.sh
@@ -2,12 +2,7 @@ mkdir -p /share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weig
 # --output-dir "/share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights" \
   # --weights "/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth" \
 
-# loc-aware: /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-07-36/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
-# simclr: /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-06-21/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
-# old yaware (lat) /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
-# old yaware (lon) /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-23_00-18-42/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
-
-# define vars for each model weight  path, s tarting with loc-aware
+# define vars for each model weight  path, starting with loc-aware
 loc_aware_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-07-36/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
 simclr_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-06-21/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
 old_yaware_lat_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
