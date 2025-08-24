c49874b42c15735e9e32a30bba28bb396ca5fe49
JuCarv-bit <juliana.souza.opp@gmail.com>
2025-08-24 17:50:37 +0200

    add pmodels path to evaluation schema

--- exploratory_notebooks/scripts/evaluate.sh
+++ exploratory_notebooks/scripts/evaluate.sh
@@ -1,8 +1,22 @@
 mkdir -p /share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights
 # --output-dir "/share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights" \
+  # --weights "/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth" \
 
+# loc-aware: /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-07-36/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
+# simclr: /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-06-21/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
+# old yaware (lat) /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
+# old yaware (lon) /share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-23_00-18-42/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth
+
+# define vars for each model weight  path, s tarting with loc-aware
+loc_aware_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-07-36/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
+simclr_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_01-06-21/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
+old_yaware_lat_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_17-07-16/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
+old_yaware_lon_weights="/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-23_00-18-42/simclr_seed42_bs256_temp0.2_Tepochs200_lr0.000375_epoch_200.pth"
+
+
+# Random approach: without weights and with output-dir
 python /share/homes/carvalhj/projects/eurosat_preprocessing/exploratory_notebooks/notebooks/main_evaluation.py \
-  --weights "/share/homes/carvalhj/projects/eurosat_preprocessing/models/2025-08-22_15-59-59/simclr_seed42_bs256_temp0.2_Tepochs2_lr0.000375_epoch_002.pth" \
+  --output-dir "/share/homes/carvalhj/projects/eurosat_preprocessing/models/random_weights2" \
   --model resnet18 \
   --dataset "eurosat" \
   --n-classes 10 \
@@ -10,3 +24,14 @@ python /share/homes/carvalhj/projects/eurosat_preprocessing/exploratory_notebook
   --batch-size 256 \
   --k 5 \
   --l2norm
+
+# Pretrained  model approach
+# python /share/homes/carvalhj/projects/eurosat_preprocessing/exploratory_notebooks/notebooks/main_evaluation.py \
+#   --weights "$loc_aware_weights" \
+#   --model resnet18 \
+#   --dataset "eurosat" \
+#   --n-classes 10 \
+#   --feature-dim 512 \
+#   --batch-size 256 \
+#   --k 5 \
+#   --l2norm
